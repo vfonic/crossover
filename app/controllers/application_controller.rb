ccb5c20481d6b00c19a8d68689ffe94afe85b5a8
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-27 18:10:29 +0800

    Don't authenticate user for every backend action...yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :authenticate_user_from_token!
-  before_filter :authenticate_user!
 
   private
 

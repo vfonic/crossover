3bf952db3e959254ad3e3118f19155da2e7cc4b7
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-28 11:13:26 +0800

    Fix app header

---
+++ frontend/app/components/app-header.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+const { service } = Ember.inject;
+
+export default Ember.Component.extend({
+  session: service('session'),
+
+  actions: {
+    logout() {
+      this.get('session').invalidate();
+    }
+  },
+});

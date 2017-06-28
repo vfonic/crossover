dbd17147ce0b0feb0ca04682f77c086cea1c2026
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-28 16:00:05 +0800

    Remove unused action

--- frontend/app/components/tickets-list.js
+++ frontend/app/components/tickets-list.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    toggleStatus(ticket){
-      if (ticket.get('status') === 'lol') {
-        ticket.set('status', 'open');
-      } else {
-        ticket.set('status', 'lol');
-      }
-      ticket.save();
-    }
-  }
 });

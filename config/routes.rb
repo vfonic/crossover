1654530beddcabbb7dba48d6a922c28b1d516d6b
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-29 05:58:20 +0800

    Fix mounting admin before frontend

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
-  mount_ember_app :frontend, to: "/"
   mount_ember_app :admin, to: "/admin"
+  mount_ember_app :frontend, to: "/"
 end

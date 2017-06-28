fec81c850547181290c0471619fc0e37c0565710
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-28 16:11:37 +0800

    Backend: Remove Admin model

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
-  devise_for :admins
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
   mount_ember_app :frontend, to: "/"
 end
